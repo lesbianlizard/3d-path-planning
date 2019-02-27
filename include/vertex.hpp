d5ac8a43032bf3ce0c0bc146b4381e8c77e2fbc6
matsumon <matsumon@oregonstate.edu>
2019-02-26 23:06:23 -0800

    just added a vertex comparison function

--- include/vertex.hpp
+++ include/vertex.hpp
@@ -15,7 +15,7 @@ class Vertex
     void setPosition(Position pos_new);
     std::vector<Vertex*> getVerticies();
     void connect(Vertex* vert_new);
-
+	bool compareVerticies(Vertex*, Vertex*);
     Vertex(x_new, y_new, z_new) : pos(x_new, y_new, z_new);
 }
 
