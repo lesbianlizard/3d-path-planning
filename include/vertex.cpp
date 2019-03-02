49d27433604803837d5e562e42067891edc0f5e4
bamberjo <32629239+bamberjo@users.noreply.github.com>
2019-03-01 17:16:43 -0800

    vertex

--- include/vertex.cpp
+++ include/vertex.cpp
@@ -4,7 +4,7 @@ bool Vertex::compareVerticies(Vertex* a,Vertex* b)
 {
 	if(a->pos.x() == b->pos.x() &&
 			a->pos.y() == b->pos.y() &&
-			a->pos.z() == b->pos.z()) 
+			a->pos.z() == b->pos.z())
 	{
 		return true;
 	}
