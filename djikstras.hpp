67498c1400632047bdbba78990a535300a3f3424
bamberjo <32629239+bamberjo@users.noreply.github.com>
2019-03-01 17:12:16 -0800

    testing

--- djikstras.hpp
+++ djikstras.hpp
@@ -9,9 +9,15 @@ class Djikstras : protected Graph
 {
   private:
     std::vector<Vertex *> uvisited;//All unvisited but 'explored' vertices
+    std::vector<Vertex *> visited;
     Vertex * curr_vert; //pointer to the current vertex of the algorithm
+    Vertex getNextUnvisited();
+    void currNeighbors(Vertex* curr_vert);//This will add neighbors to the unvisited array
+    void removeUnivisited(Vertex* vert);
+    void addUnivisited(Vertex* vert);
+    void addVisited(Vertex* vert);
   public:
 
-    Vertex getNextUnvisited();
-    void currNeighbors(Vertex* curr_vert);//This will set the weights/parents of all of the n
+    std::vector<Vertex *> getPath(Vertex * vertex_start, Vertex * vertex_end);
+    std::vector<Vertex *> getPath(std::vector<Vertex *>); // Overloaded function to get path for many waypoints
 }
--- vertex.hpp
+++ vertex.hpp
@@ -2,19 +2,25 @@
 #define VERTEX_HPP
 
 #include <vector>
+#include "config.hpp"
 
 class Vertex
 {
   private:
     Position pos;
+    FLOAT_TYPE weight;//Weight of the node in the graph for djikstras
+    Vertex * parentVertex;
     std::vector<Vertex*> points_to;
   public:
     double distanceTo(Vertex* vertex);
 
+    FLOAT_TYPE getWeight();
     Position getPosition();
     void setPosition(Position pos_new);
     std::vector<Vertex*> getVerticies();
-    void connect(Vertex* vert_new);
+    void addVertex(Vertex* vert_new);
+    std::vector<Vertex*> setNeighbors();//returns neighbors that were set
+
 }
 
 #endif
